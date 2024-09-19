ff6c0ae19b30d8bbdb80fe7ab6d390ce375f3bc5
MavekeJoy <Mavekejoy@gmail.com>
2024-09-19 19:10:23 +0300

    Final Commit

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+
 
 from sqlalchemy import (Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,3 +11,10 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def __repr__(self):
+        return "<Dog(name='%s', breed='%s')>" % (self.name, self.breed) 
